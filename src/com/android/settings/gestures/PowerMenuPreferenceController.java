PixelExperience/packages_apps_Settings

Settings: Import statusbar and others customizations
Squashed of:

commit 1a57349b81a37a66f91e7e33da60147ce9880144
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Wed Mar 3 16:55:32 2021 -0300

    Implement statusbar settings

    Based on https://github.com/LineageOS/android_packages_apps_LineageParts

    Change-Id: Ib1fb275e0016dc3c1da5488ce343c6fdb151f1df

commit 327ce2f703eafc2cdee5aa4af74efeea8b970d33
Author: jhenrique09 <jhsv09@gmail.com>
Date:   Fri Mar 25 16:57:58 2022 +0000

    StatusBarSettings: Fix padding on some preferences

    Change-Id: I313576b20ee12dc1cb97887cb12b49e66e4e1381

commit 5bd5045102287f268a5eefc299a2bfe8e9b33d93
Author: jhenrique09 <jhsv09@gmail.com>
Date:   Fri Mar 25 17:02:03 2022 +0000

    StatusBarSettings: Remove some preferences

    Change-Id: Ia6933fbd251ae7fe06ab93a0208b141e8d1ab6e0

commit 29c0fe93e4831c03e688ffab0041c48cc8ef523e
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Thu Mar 4 14:54:30 2021 -0300

    ButtonSettings: Implement the remaining functions

    Change-Id: I92b600f4c0bfdfb3fe9784541abe9bfa0abbd312

commit 8d4e3ec877d04336d07dcd726ffc27ca2572d68c
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Thu Mar 4 17:15:14 2021 -0300

    ButtonSettings: Add shortcut for power menu settings

    Change-Id: I2c8754de757a54a4e8877457ca8a9f429f9c2f07

commit 257a1760a752d6f207f42574c178727b54edc1a7
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Thu Mar 4 23:07:10 2021 -0300

    Gestures: Add settings activity for legacy navigation

    Change-Id: I0afb86596666d469952d9bbbe64dfa4f5ab23ad2

commit 69af54bac6fa46e959884ef7cfc2e04eb3a1f2e4
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Thu Mar 4 23:08:39 2021 -0300

    ButtonSettings: Remove delay when toggling navbar invert layout

    It seems not needed

    Change-Id: Ic9576d0caf7455ba9a300a9282e3c7f218e89562

commit 685277d39e7cddb6e158b4f0e65615c2d1b24884
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   

src/com/android/settings/gestures/PowerMenuPreferenceController.java
@@ -32,14 +32,18 @@ public class PowerMenuPreferenceController extends BasePreferenceController {
 
     @Override
     public CharSequence getSummary() {
-        final int powerButtonValue = PowerMenuSettingsUtils.getPowerButtonSettingValue(mContext);
+        return getPrefSummary(mContext);
+    }
+
+    public static String getPrefSummary(Context context){
+        final int powerButtonValue = PowerMenuSettingsUtils.getPowerButtonSettingValue(context);
         if (powerButtonValue == LONG_PRESS_POWER_ASSISTANT_VALUE) {
-            return mContext.getText(R.string.power_menu_summary_long_press_for_assist_enabled);
+            return context.getString(R.string.power_menu_summary_long_press_for_assist_enabled);
         } else if (powerButtonValue == LONG_PRESS_POWER_GLOBAL_ACTIONS) {
-            return mContext.getText(
+            return context.getString(
                     R.string.power_menu_summary_long_press_for_assist_disabled_with_power_menu);
         } else {
-            return mContext.getText(
+            return context.getString(
                     R.string.power_menu_summary_long_press_for_assist_disabled_no_action);
         }
     }

Nabu-upsidedowncake/android_packages_apps_Settings

src/com/android/settings/gestures/PowerMenuPreferenceController.java
@@ -29,10 +29,14 @@ public class PowerMenuPreferenceController extends BasePreferenceController {
 
     @Override
     public CharSequence getSummary() {
-        if (PowerMenuSettingsUtils.isLongPressPowerForAssistantEnabled(mContext)) {
-            return mContext.getText(R.string.power_menu_summary_long_press_for_assistant);
+        return getPrefSummary(mContext);
+    }
+
+    public static String getPrefSummary(Context context){
+        if (PowerMenuSettingsUtils.isLongPressPowerForAssistantEnabled(context)) {
+            return context.getString(R.string.power_menu_summary_long_press_for_assistant);
         } else {
-            return mContext.getText(R.string.power_menu_summary_long_press_for_power_menu);
+            return context.getString(R.string.power_menu_summary_long_press_for_power_menu);
         }
     }
 