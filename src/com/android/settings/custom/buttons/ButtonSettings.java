LineageOS/android_packages_apps_LineageParts

Implement edge long swipe gesture [3/3]
Add long swipe action list preference in Button Settings.

Change-Id: I39bd5b9dc779ca4b827fda2673252529333ab05f

src/org/lineageos/lineageparts/input/ButtonSettings.java
@@ -78,6 +78,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_NAVIGATION_HOME_DOUBLE_TAP = "navigation_home_double_tap";
     private static final String KEY_NAVIGATION_APP_SWITCH_LONG_PRESS =
             "navigation_app_switch_long_press";
+    private static final String KEY_EDGE_LONG_SWIPE = "navigation_bar_edge_long_swipe";
     private static final String KEY_POWER_END_CALL = "power_end_call";
     private static final String KEY_HOME_ANSWER_CALL = "home_answer_call";
     private static final String KEY_VOLUME_MUSIC_CONTROLS = "volbtn_music_controls";
@@ -117,6 +118,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mNavigationHomeLongPressAction;
     private ListPreference mNavigationHomeDoubleTapAction;
     private ListPreference mNavigationAppSwitchLongPressAction;
+    private ListPreference mEdgeLongSwipeAction;
     private SwitchPreference mPowerEndCall;
     private SwitchPreference mHomeAnswerCall;
     private SwitchPreference mTorchLongPressPowerGesture;
@@ -212,6 +214,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         Action appSwitchLongPressAction = Action.fromSettings(resolver,
                 LineageSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 defaultAppSwitchLongPressAction);
+        Action edgeLongSwipeAction = Action.fromSettings(resolver,
+                LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION,
+                Action.NOTHING);
 
         // Navigation bar home long press
         mNavigationHomeLongPressAction = initList(KEY_NAVIGATION_HOME_LONG_PRESS,
@@ -224,6 +229,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         // Navigation bar app switch long press
         mNavigationAppSwitchLongPressAction = initList(KEY_NAVIGATION_APP_SWITCH_LONG_PRESS,
                 appSwitchLongPressAction);
+
+        // Edge long swipe gesture
+        mEdgeLongSwipeAction = initList(KEY_EDGE_LONG_SWIPE, edgeLongSwipeAction);
 
         final LineageHardwareManager hardware = LineageHardwareManager.getInstance(getActivity());
 
@@ -469,6 +477,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
             mNavigationAppSwitchLongPressAction.setEntries(actionEntriesGo);
             mNavigationAppSwitchLongPressAction.setEntryValues(actionValuesGo);
+
+            mEdgeLongSwipeAction.setEntries(actionEntriesGo);
+            mEdgeLongSwipeAction.setEntryValues(actionValuesGo);
         }
     }
 
@@ -569,6 +580,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             handleListChange(mTorchLongPressPowerTimeout, newValue,
                     LineageSettings.System.TORCH_LONG_PRESS_POWER_TIMEOUT);
             return true;
+        } else if (preference == mEdgeLongSwipeAction) {
+            handleListChange(mEdgeLongSwipeAction, newValue,
+                    LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION);
+            return true;
         }
         return false;
     }
@@ -615,10 +630,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 mNavigationPreferencesCat.addPreference(mNavigationHomeLongPressAction);
                 mNavigationPreferencesCat.addPreference(mNavigationHomeDoubleTapAction);
                 mNavigationPreferencesCat.addPreference(mNavigationAppSwitchLongPressAction);
+                mNavigationPreferencesCat.addPreference(mEdgeLongSwipeAction);
             } else {
                 mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
                 mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
                 mNavigationPreferencesCat.removePreference(mNavigationAppSwitchLongPressAction);
+                mNavigationPreferencesCat.removePreference(mEdgeLongSwipeAction);
             }
         }
         if (homeCategory != null) {

Nabu-upsidedowncake/android_packages_apps_Settings

src/com/android/settings/custom/buttons/ButtonSettings.java
@@ -95,6 +95,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mAssistLongPressAction;
     private ListPreference mAppSwitchPressAction;
     private ListPreference mAppSwitchLongPressAction;
+    private ListPreference mEdgeLongSwipeAction;
     private SwitchPreference mCameraWakeScreen;
     private SwitchPreference mCameraSleepOnRelease;
     private SwitchPreference mCameraLaunch;
@@ -118,6 +119,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
     private static final String KEY_VOLUME_MUSIC_CONTROLS = "volbtn_music_controls";
     private static final String KEY_GESTURE_POWER_MENU = "gesture_power_menu_summary";
+    private static final String KEY_EDGE_LONG_SWIPE = "navigation_bar_edge_long_swipe";
 
     private static final String CATEGORY_VOLUME = "volume_keys";
     private static final String CATEGORY_BACKLIGHT = "button_backlight_cat";
@@ -242,6 +244,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         Action appSwitchLongPressAction = Action.fromSettings(resolver,
                 Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 defaultAppSwitchLongPressAction);
+        Action edgeLongSwipeAction = Action.fromSettings(resolver,
+                Settings.System.KEY_EDGE_LONG_SWIPE_ACTION,
+                Action.NOTHING);
+
+        // Edge long swipe gesture
+        mEdgeLongSwipeAction = initList(KEY_EDGE_LONG_SWIPE, edgeLongSwipeAction);
 
         // Only visible on devices that does not have a navigation bar already
         if (NavbarUtils.canDisable(getActivity())) {
@@ -252,6 +260,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             mNavigationInverse.setDependency(DISABLE_NAV_KEYS);
             mNavigationGestures.setDependency(DISABLE_NAV_KEYS);
             mNavigationCompactLayout.setDependency(DISABLE_NAV_KEYS);
+            mEdgeLongSwipeAction.setDependency(DISABLE_NAV_KEYS);
         } else {
             mNavbarCategory.removePreference(mDisableNavigationKeys);
             mDisableNavigationKeys = null;
@@ -442,6 +451,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         // Others preferences
         updateOthersSummaries();
+
+        // Edge swipe gesture
+        updateEdgeSwipeGesturePreference();
     }
 
     private static boolean isKeySwapperSupported(Context context) {
@@ -464,6 +476,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             if (mNavigationCompactLayout != null){
                 mNavbarCategory.removePreference(mNavigationCompactLayout);
                 mNavigationCompactLayout = null;
+            }
+        }else{
+            if (mEdgeLongSwipeAction != null){
+                mNavbarCategory.removePreference(mEdgeLongSwipeAction);
+                mEdgeLongSwipeAction = null;
             }
         }
         if (!SystemNavigationPreferenceController.isGestureAvailable(getActivity())){
@@ -523,11 +540,24 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         // Others preferences
         updateOthersSummaries();
+
+        // Edge swipe gesture
+        updateEdgeSwipeGesturePreference();
     }
 
     private void updateOthersSummaries(){
         if (mGesturePowerMenu != null){
             mGesturePowerMenu.setSummary(PowerMenuPreferenceController.getPrefSummary(getActivity()));
+        }
+    }
+
+    private void updateEdgeSwipeGesturePreference(){
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (mEdgeLongSwipeAction != null){
+            mEdgeLongSwipeAction.setValue(Integer.toString(Action.fromSettings(resolver,
+                Settings.System.KEY_EDGE_LONG_SWIPE_ACTION,
+                Action.NOTHING).ordinal()));
+            mEdgeLongSwipeAction.setSummary(mEdgeLongSwipeAction.getEntry());
         }
     }
 
@@ -613,6 +643,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         }else if (preference == mVolumeKeyCursorControl) {
             handleListChange(mVolumeKeyCursorControl, newValue,
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL);
+            return true;
+        } else if (preference == mEdgeLongSwipeAction) {
+            handleListChange(mEdgeLongSwipeAction, newValue,
+                    Settings.System.KEY_EDGE_LONG_SWIPE_ACTION);
             return true;
         }
         return false;