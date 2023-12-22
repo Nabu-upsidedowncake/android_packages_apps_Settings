bezke/packages_apps_Settings_derp

Settings: Add restart systemui shortcut to power menu [2/2]
Change-Id: I9576d599c38d343cb7daa1ae88714b740be9d2d4
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/android/settings/derp/buttons/PowerMenuActions.java
@@ -65,6 +65,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mEmergencyPref;
     private CheckBoxPreference mDeviceControlsPref;
     private CheckBoxPreference mPanicPref;
+    private CheckBoxPreference mRestartSystemUIPref;
 
     private LineageGlobalActions mLineageGlobalActions;
 
@@ -105,6 +106,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
             } else if (action.equals(GLOBAL_ACTION_KEY_PANIC)) {
                 mPanicPref = findPreference(GLOBAL_ACTION_KEY_PANIC);
+            } else if (action.equals(GLOBAL_ACTION_KEY_RESTART_SYSTEMUI)) {
+                mRestartSystemUIPref = findPreference(GLOBAL_ACTION_KEY_RESTART_SYSTEMUI);
             }
         }
 
@@ -178,6 +181,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mPanicPref.setEnabled(PowerMenuUtils.isPanicAvailable(mContext));
         }
 
+        if (mRestartSystemUIPref != null) {
+            mRestartSystemUIPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_RESTART_SYSTEMUI));
+        }
+
         updatePreferences();
     }
 
@@ -220,6 +228,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mPanicPref) {
             value = mPanicPref.isChecked();
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_PANIC);
+
+        } else if (preference == mRestartSystemUIPref) {
+            value = mRestartSystemUIPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_RESTART_SYSTEMUI);
 
         } else {
             return super.onPreferenceTreeClick(preference);

Nabu-upsidedowncake/android_packages_apps_Settings

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

src/com/android/settings/custom/buttons/PowerMenuActions.java
@@ -63,6 +63,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mEmergencyPref;
     private CheckBoxPreference mDeviceControlsPref;
     private CheckBoxPreference mPanicPref;
+    private CheckBoxPreference mRestartSystemUIPref;
 
     private CustomGlobalActions mCustomGlobalActions;
 
@@ -100,6 +101,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
             } else if (action.equals(GLOBAL_ACTION_KEY_PANIC)) {
                 mPanicPref = findPreference(GLOBAL_ACTION_KEY_PANIC);
+            } else if (action.equals(GLOBAL_ACTION_KEY_RESTART_SYSTEMUI)) {
+                mRestartSystemUIPref = findPreference(GLOBAL_ACTION_KEY_RESTART_SYSTEMUI);
             }
         }
 
@@ -168,6 +171,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mPanicPref.setEnabled(PowerMenuUtils.isPanicAvailable(mContext));
         }
 
+        if (mRestartSystemUIPref != null) {
+            mRestartSystemUIPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_RESTART_SYSTEMUI));
+        }
+
         updatePreferences();
     }
 
@@ -204,6 +212,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mPanicPref) {
             value = mPanicPref.isChecked();
             mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_PANIC);
+
+        } else if (preference == mRestartSystemUIPref) {
+            value = mRestartSystemUIPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_RESTART_SYSTEMUI);
 
         } else {
             return super.onPreferenceTreeClick(preference);